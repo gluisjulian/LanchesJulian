8400b1f289d66a84461992eb7e1ef4adaaf9149b
Gabriel Julian <gluisjulian@gmail.com>
2026-05-27 21:30:32 -0300

    Ajustando codigo para exibir lanches

---
+++ LanchesJulian/Controllers/LancheController.cs
@@ -0,0 +1,23 @@
+﻿using LanchesJulian.Context;
+using LanchesJulian.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesJulian.Controllers
+{
+    public class LancheController : Controller
+    {
+        private readonly ILancheRepository _repository;
+
+        public LancheController(ILancheRepository repository)
+        {
+            _repository = repository;
+        }
+
+
+        public IActionResult List()
+        {
+            var lanches = _repository.Lanches;
+            return View(lanches);
+        }
+    }
+}
